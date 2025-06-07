e461b8300ea44fd69e6e7b4441fd7c31fabb3714
yasmina-bonney <yasbonney@gmail.com>
2025-06-07 21:57:10 +0000

    updated immutable and cow list without copy module

--- modules/lists/cow_list.py
+++ modules/lists/cow_list.py
@@ -1,4 +1,4 @@
-import copy
+
 
 class CowList:
     def __init__(self, input_list):
@@ -8,11 +8,15 @@ class CowList:
         return self.backing_list[i]   
 
     def append(self, item):
-        modified_list = copy.deepcopy(self.backing_list)
+        modified_list = []
+        for i in self.backing_list:
+            modified_list.append(i)
         modified_list.append(item)
         return CowList(modified_list)
 
     def set_value(self, i, value):
-        updated_list = copy.deepcopy(self.backing_list)
+        updated_list = []
+        for k in self.backing_list:
+            updated_list.append(k)       
         updated_list[i] = value
         return CowList(updated_list)
--- modules/lists/immutable_list.py
+++ modules/lists/immutable_list.py
@@ -1,17 +1,22 @@
-import copy
+
 
 class ImmutableList:
     def __init__(self, input_list):
-        self.backing_list = input_list
-        self.updated_list = copy.deepcopy(self.backing_list)
+        updated_list = []
+        #copying input_list
+        for i in input_list:
+            updated_list.append(i)
+        self.backing_list = updated_list
 
     def append(self, item):
-        modified_list = copy.deepcopy(self.updated_list)
+        modified_list = []
+        for i in self.backing_list:
+            modified_list.append(i)
         modified_list.append(item)
         return ImmutableList(modified_list)
 
     def __getitem__(self, i):
-        return self.updated_list[i]
+        return self.backing_list[i]
     
     ##doesn't the concept of __setitem__ defeat the purpose of immutablity here?
     def __setitem__(self, i, value):
