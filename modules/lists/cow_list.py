d7f3b19a957b502b4be1627f513bb1294193150e
yasmina-bonney <yasbonney@gmail.com>
2025-06-08 10:36:57 +0000

    updates copy

--- modules/lists/cow_list.py
+++ modules/lists/cow_list.py
@@ -2,21 +2,17 @@
 
 class CowList:
     def __init__(self, input_list):
-        self.backing_list = input_list
+        self.backing_list = input_list[:]
         
     def __getitem__(self, i):
         return self.backing_list[i]   
 
     def append(self, item):
-        modified_list = []
-        for i in self.backing_list:
-            modified_list.append(i)
+        modified_list = self.backing_list[:]
         modified_list.append(item)
         return CowList(modified_list)
 
     def set_value(self, i, value):
-        updated_list = []
-        for k in self.backing_list:
-            updated_list.append(k)       
+        updated_list = self.backing_list[:]     
         updated_list[i] = value
         return CowList(updated_list)
