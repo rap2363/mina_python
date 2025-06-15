0c9864f9db2105d2a5a9647376625c988194f421
yasmina-bonney <yasbonney@gmail.com>
2025-06-15 10:39:56 +0000

    defines equality and hash

--- modules/algorithm/puzzle_15_bfs.py
+++ modules/algorithm/puzzle_15_bfs.py
@@ -1,6 +1,13 @@
 class State:
     def __init__(self, initial_state):
-        self.initial_state = initial_state
+        self.initial_state = tuple(initial_state)
+
+    def __eq__(self, other):
+        return self.initial_state == other.initial_state
+    
+    def __hash__(self):
+        return hash(tuple(self.initial_state))
+    
         
     def generate_neighbours(self):
         first_state = self.initial_state
@@ -26,34 +33,34 @@ class State:
             if direction == 'right' and index in invalid_right:
                 continue
             if 0 <= new_index < 9:
-                new_initial_state = first_state[:]
+                new_initial_state = list(first_state) # converting to list to modify
                 new_initial_state[index],new_initial_state[new_index] =  new_initial_state[new_index], new_initial_state[index]
-                neighbour.append(new_initial_state)
+                neighbour.append(tuple(new_initial_state)) #making it hashable
         return neighbour
 
-    def breadth_first_search(self,initial_state,goal_state):
+    def breadth_first_search(self,goal_state):
 
         queue = [] # or could be a deque
-        queue.append(initial_state)
-        visited_states = set()
-        visited_states.add(tuple(initial_state))
+        queue.append(self.initial_state)
+        visited_states = set([])
+        visited_states.add(self.initial_state)
         steps = {}  # To track the different states
 
         while len(queue) > 0:
             current_state = queue.pop(0)
-            if current_state == goal_state:
+            if current_state == tuple(goal_state):
                 print(f'The puzzule has been sovle. {current_state}')
                 return current_state
             
             current = State(current_state)
             neighbors = current.generate_neighbours()
             for n in neighbors:
-                    if tuple(n) not in visited_states:
+                    if n not in visited_states:
                         
-                        visited_states.add(tuple(n))
+                        visited_states.add(n)
                         queue.append(n)
             
         print(f'Puzzle not solvable')
-        return initial_state
-    
+
+        return self.initial_state
     
--- modules/algorithm/test_puzzle_15_bfs.py
+++ modules/algorithm/test_puzzle_15_bfs.py
@@ -6,14 +6,14 @@ def test_breadth_first_search():
     goal_state = [1,2,3,4,5,6,7,8,0]
 
     initial_state = State(x)
-    result = initial_state.breadth_first_search(x,goal_state)
+    result = initial_state.breadth_first_search(goal_state)
 
-    assert result == goal_state
+    assert result == tuple(goal_state)
 
     y = [1,2,3,0,4,5,7,8,6]
     goal_state = [1,2,3,4,5,6,7,9,0]
 
     initial_state = State(y)
-    result = initial_state.breadth_first_search(y,goal_state)
+    result = initial_state.breadth_first_search(goal_state)
 
-    assert result == y
+    assert result == tuple(y)
